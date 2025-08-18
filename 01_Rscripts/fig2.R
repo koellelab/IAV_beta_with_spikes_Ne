56f4f36867837038c10538dc7d24e4822553b754
Yike (Teresa) Shi <ys0286@princeton.edu>
2025-08-18 12:08:31 -0400

    Teresa's update

---
+++ 01_Rscripts/fig2.R
@@ -0,0 +1,135 @@
+# 1. Source functions ====
+source("01_Rscripts/WithinhostIAV_Ne_fnuctions_log.R")
+
+# 2. Load packages ====
+library(tidyverse)
+library(ggplot2)
+library(patchwork)
+library(showtext)
+library(colorspace)
+library(parallel)
+theme_set(theme_minimal() +
+            theme(axis.ticks = element_line(colour = "grey50"),
+                  axis.line = element_line(colour = "grey50"),
+                  axis.text = element_text(size = 12),
+                  axis.title = element_text(size = 15),
+                  legend.text = element_text(size = 12)))
+
+# 3. Input parameters ====
+Ne_values <- seq(10, 100, 1)
+mu <- 0
+alpha <- 0.02
+hourPerGen <- 6
+m <- 0.004
+freq_step <- 0.01
+
+# 4. Input datasets ====
+McCrone_IAV <- read_csv("03_data/table_S1.csv") %>%
+  set_names(c("ENROLLID", "mutation", "DPS1", "DPS2", "freq1", "freq2", "Subset")) %>%
+  mutate(gen = (DPS2 - DPS1)*(24/hourPerGen)) %>%
+  filter(Subset == 1)
+
+# 5. Generate data frame ====
+## 5.1. Figure 2A ====
+# data frame only including first round of observations
+McCrone_obs1 <- McCrone_IAV %>%
+  select(mutation, ENROLLID, DPS1, freq1, gen)
+McCrone_obs1$type <- c("obs1")
+names(McCrone_obs1) <- c("mutation", "ENROLLID", "DPS", "freq", "gen", "type")
+# data frame only including second round of observations
+McCrone_obs2 <- McCrone_IAV %>%
+  select(mutation, ENROLLID, DPS2, freq2, gen)
+McCrone_obs2$type <- c("obs2")
+names(McCrone_obs2) <- c("mutation", "ENROLLID", "DPS", "freq", "gen", "type")
+# merge the data together
+McCrone_tot_obs <- rbind(McCrone_obs1, McCrone_obs2)
+McCrone_tot_obs[McCrone_tot_obs$freq == 0, "type"] <- "observation 2 below 2%"
+McCrone_tot_obs[McCrone_tot_obs$freq == 0, "freq"] <- 0.02
+McCrone_tot_obs$type <- factor(McCrone_tot_obs$type, labels = c("observation 1", "observation 2", "observation 2 below 2%"))
+
+## 5.2. Figure 2B ====
+LLdf_McCrone <- LL_Ne.df(McCrone_IAV, Ne_values, mu, alpha, hourPerGen, m)
+MLE_Ne_McCrone <- LLdf_McCrone$Ne[which.max(LLdf_McCrone$prob)]
+CI_low_Ne <- LLdf_McCrone %>%
+  filter(Ne < MLE_Ne_McCrone) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+CI_high_Ne <- LLdf_McCrone %>%
+  filter(Ne > MLE_Ne_McCrone) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+
+## 5.4. Figure 2C ====
+# create empty list
+list_df_sim_bs <- list()
+for (i in 1:1000) {
+  list_df_sim_bs[[i]] <- data.frame(DPS1 = McCrone_IAV$DPS1, 
+                                    DPS2 = McCrone_IAV$DPS2, 
+                                    freq1 = McCrone_IAV$freq1, 
+                                    freq2 = 0)
+}
+
+# generate mock dataset
+for (i in 1:nrow(McCrone_IAV)) {
+  ini_p <- McCrone_IAV$freq1[i] 
+  gen <- McCrone_IAV$gen[i]
+  v <- 0.004*ini_p*(1-ini_p)
+  mock_df_bs <- beta_spike_threshold(MLE_Ne_McCrone, ini_p, gen, mu, freq_step, v, alpha)
+  for (j in 1:1000) {
+    random_num <- runif(1)
+    mock_beta_spike <- mock_df_bs[which.min(abs(mock_df_bs$cdf - random_num)),]
+    list_df_sim_bs[[j]]$freq2[i] <- mock_beta_spike$freq
+  }
+}
+
+# calculate log-likelihood
+list_ll_max <- 1:length(list_df_sim_bs) %>%
+  mclapply(function(x) {
+    list_ll_NE <- data.frame()
+    mock_ll_df <- LL_Ne.df(list_df_sim_bs[[x]], seq(10, 100, 1), mu, alpha, hourPerGen, m)
+    list_ll_NE <- rbind(list_ll_NE, mock_ll_df[which.max(mock_ll_df$prob), ])
+    return(list_ll_NE)
+  }, mc.cores = detectCores())
+list_ll_max <- do.call(rbind, list_ll_max)
+
+# 6. Graphs ====
+fig2a <- ggplot(McCrone_tot_obs, aes(x = DPS, y = freq)) +
+  geom_hline(yintercept = 0.02, color = "red", linetype = "dashed", ) +
+  geom_point(aes(color = type, shape = type), alpha = 0.5) +
+  geom_line(aes(group = ENROLLID), alpha = 0.3) +
+  scale_shape_manual(values = c(19, 17, 2))+
+  scale_color_manual(values = c("#26185F", "#0095AF", "black"))+
+  labs(x = "Days post symptom onset", y = "iSNV frequency", color = NULL, shape = NULL) +
+  theme(legend.position = c(0.3, 0.93)) +
+  ylim(0, 0.7)
+
+fig2b <- ggplot(LLdf_McCrone) +
+  geom_line(aes(x = Ne, y = prob)) +
+  geom_vline(xintercept = MLE_Ne_McCrone, color = "red") +
+  geom_hline(yintercept = max(LLdf_McCrone$prob), color = "red", linetype = "dashed") +
+  annotate("text", x = 70, y = -38, label = paste("N[E] == ", MLE_Ne_McCrone), parse = TRUE, color = "red") +
+  annotate("rect", xmin = as.numeric(CI_low_Ne), xmax = as.numeric(CI_high_Ne), 
+           ymin = -Inf, ymax = Inf, alpha = 0.1,fill = "black") +
+  labs(x = expression(N[E]), y = "Log-likelihood") +
+  scale_x_continuous(breaks = seq(0, 100, 20), limits = c(0, 101))
+
+fig2c <- ggplot(list_ll_max, aes(x = prob)) +
+  geom_histogram(bins = 60, fill = "#005D9E") +
+  geom_vline(xintercept = max(LLdf_McCrone$prob), color = "red", linetype = "dashed") +
+  labs(x = "Log-likelihood", y = "Count")
+
+fig2d <- ggplot(list_ll_max, aes(x = Ne)) +
+  geom_bar(fill = "#005D9E") +
+  geom_vline(xintercept = MLE_Ne_McCrone, color = "red") +
+  annotate("rect", xmin = as.numeric(CI_low_Ne), xmax = as.numeric(CI_high_Ne), 
+           ymin = -Inf, ymax = Inf, alpha = 0.1,fill = "black") +
+  labs(x = expression(N[E]), y = "Count") +
+  scale_x_continuous(breaks = seq(0, 100, 20), limits = c(0, 101))
+
+# 7. Put graphs together ====
+fig2 <- fig2a + fig2b + fig2c + fig2d +
+  plot_layout(ncol = 2) +
+  plot_annotation(tag_levels = "A") &
+  theme(plot.tag = element_text(face = "bold", size = 18))
+
+ggsave(fig2, file = "fig2_log.pdf", path = "02_plots", width = 10, height = 8)
---
+++ 01_Rscripts/fig3.R
@@ -0,0 +1,159 @@
+# 1. Source functions ====
+source("01_Rscripts/WithinhostIAV_Ne_fnuctions.R")
+
+# 2. Load packages ====
+library(tidyverse)
+library(ggplot2)
+library(patchwork)
+library(showtext)
+library(colorspace)
+library(parallel)
+theme_set(theme_minimal() +
+            theme(axis.ticks = element_line(colour = "grey50"),
+                  axis.line = element_line(colour = "grey50"),
+                  axis.text = element_text(size = 15),
+                  axis.title = element_text(size = 18),
+                  legend.text = element_text(size = 15),
+                  plot.title = element_text(size = 20)))
+
+# 3. Input parameters ====
+mu <- 0 #2 * 10^(-4)
+hourPerGen <- 6
+m <- 0.004
+set.seed(12)
+
+# 4. Input datasets ====
+McCrone_IAV_sub2 <- read_csv("03_data/table_S1.csv") %>%
+  set_names(c("ENROLLID", "mutation", "DPS1", "DPS2", "freq1", "freq2", "Subset")) %>%
+  mutate(gen = (DPS2 - DPS1)*(24/hourPerGen)) %>%
+  filter(Subset == 2)
+
+# 5. Generate data frame ====
+## 5.1. 0.02 ====
+McCrone_IAV_freq0.02 <- McCrone_IAV_sub2
+McCrone_IAV_freq0.02$freq1 <- 0.02
+McCrone_IAV_freq0.02_gen1 <- McCrone_IAV_freq0.02
+McCrone_IAV_freq0.02_gen1$DPS1 <- McCrone_IAV_freq0.02_gen1$DPS2 - 0.25
+McCrone_tot_obs_freq0.02 <- adjust_data(McCrone_IAV_freq0.02)
+
+McCrone_tot_obs_freq0.02_gen1 <- adjust_data(McCrone_IAV_freq0.02_gen1)
+
+LLdf_McCrone_freq0.02 <- LL_Ne.df(McCrone_IAV_freq0.02, seq(50, 1000, 5), mu, 0.02, hourPerGen, m)
+LLdf_McCrone_freq0.02$prob <- LLdf_McCrone_freq0.02$prob - log(1 - 0.02)
+MLE_Ne_McCrone_freq0.02 <- LLdf_McCrone_freq0.02$Ne[which.max(LLdf_McCrone_freq0.02$prob)]
+CI_low_Ne_freq0.02 <- LLdf_McCrone_freq0.02 %>%
+  filter(Ne < MLE_Ne_McCrone_freq0.02) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+CI_high_Ne_freq0.02 <- LLdf_McCrone_freq0.02 %>%
+  filter(Ne > MLE_Ne_McCrone_freq0.02) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+
+LLdf_McCrone_freq0.02_gen1 <- LL_Ne.df(McCrone_IAV_freq0.02_gen1, seq(15, 150, 1), mu, 0.02, 24, m)
+LLdf_McCrone_freq0.02_gen1$prob <- LLdf_McCrone_freq0.02_gen1$prob - log(1 - 0.02)
+MLE_Ne_McCrone_freq0.02_gen1 <- LLdf_McCrone_freq0.02_gen1$Ne[which.max(LLdf_McCrone_freq0.02_gen1$prob)]
+CI_low_Ne_freq0.02_gen1 <- LLdf_McCrone_freq0.02_gen1 %>%
+  filter(Ne < MLE_Ne_McCrone_freq0.02_gen1) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+CI_high_Ne_freq0.02_gen1 <- LLdf_McCrone_freq0.02_gen1 %>%
+  filter(Ne > MLE_Ne_McCrone_freq0.02_gen1) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+
+## 5.2. 0.005 ====
+McCrone_IAV_freq0.005 <- McCrone_IAV_sub2
+McCrone_IAV_freq0.005$freq1 <- 0.005
+McCrone_IAV_freq0.005_gen1 <- McCrone_IAV_freq0.005
+McCrone_IAV_freq0.005_gen1$DPS2 <- McCrone_IAV_freq0.005_gen1$DPS1 + 1
+McCrone_tot_obs_freq0.005 <- adjust_data(McCrone_IAV_freq0.005)
+
+LLdf_McCrone_freq0.005 <- LL_Ne.df(McCrone_IAV_freq0.005, seq(50,400,2), mu, 0.005, hourPerGen, m)
+LLdf_McCrone_freq0.005$prob <- LLdf_McCrone_freq0.005$prob - log(1 - 0.005)
+MLE_Ne_McCrone_freq0.005 <- LLdf_McCrone_freq0.005$Ne[which.max(LLdf_McCrone_freq0.005$prob)]
+CI_low_Ne_freq0.005 <- LLdf_McCrone_freq0.005 %>%
+  filter(Ne < MLE_Ne_McCrone_freq0.005) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+CI_high_Ne_freq0.005 <- LLdf_McCrone_freq0.005 %>%
+  filter(Ne > MLE_Ne_McCrone_freq0.005) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+
+LLdf_McCrone_freq0.005_gen1 <- LL_Ne.df(McCrone_IAV_freq0.005_gen1, seq(25,40,0.5), mu, 0.005, 24, m)
+LLdf_McCrone_freq0.005_gen1$prob <- LLdf_McCrone_freq0.005_gen1$prob - log(1 - 0.005)
+MLE_Ne_McCrone_freq0.005_gen1 <- LLdf_McCrone_freq0.005_gen1$Ne[which.max(LLdf_McCrone_freq0.005_gen1$prob)]
+CI_low_Ne_freq0.005_gen1 <- LLdf_McCrone_freq0.005_gen1 %>%
+  filter(Ne < MLE_Ne_McCrone_freq0.005_gen1) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+CI_high_Ne_freq0.005_gen1 <- LLdf_McCrone_freq0.005_gen1 %>%
+  filter(Ne > MLE_Ne_McCrone_freq0.005_gen1) %>%
+  slice(which.min(abs(prob - (max(prob)-1.92)))) %>%
+  select(Ne)
+
+# 6. Graphs ====
+fig3a <- ggplot(McCrone_tot_obs_freq0.02, aes(x = DPS, y = freq)) +
+  geom_point(aes(color = type, shape = type), alpha = 0.5) +
+  geom_line(aes(group = ID), alpha = 0.3) +
+  geom_hline(yintercept = 0.02, color = "red", linetype = "dashed", ) +
+  scale_color_discrete_sequential(palette = "YlGnBu", nmax = 3, rev = FALSE) +
+  labs(x = "Days post symptom onset", y = "iSNV frequency", color = NULL, shape = NULL) +
+  theme(legend.position = c(0.25, 0.9)) +
+  ylim(0, 0.4) +
+  xlim(-2, 6)
+
+fig3b <- ggplot(LLdf_McCrone_freq0.02) +
+  geom_line(aes(x = Ne, y = prob)) +
+  geom_vline(xintercept = MLE_Ne_McCrone_freq0.02, color = "red") +
+  geom_hline(yintercept = max(LLdf_McCrone_freq0.02$prob), color = "red", linetype = "dashed") +
+  annotate("text", x = 750, y = -65, label = paste("N[E] == ", MLE_Ne_McCrone_freq0.02), parse = TRUE, color = "red", size = 6) +
+  annotate("rect", xmin = as.numeric(CI_low_Ne_freq0.02), xmax = as.numeric(CI_high_Ne_freq0.02), 
+           ymin = -Inf, ymax = Inf, alpha = 0.1,fill = "black") +
+  labs(x = expression(N[E]), y = "Log-likelihood", title = expression(paste(threshold, "= 2%"))) 
+
+fig3c <- ggplot(LLdf_McCrone_freq0.005) +
+  geom_line(aes(x = Ne, y = prob)) +
+  geom_vline(xintercept = MLE_Ne_McCrone_freq0.005, color = "red") +
+  geom_hline(yintercept = max(LLdf_McCrone_freq0.005$prob), color = "red", linetype = "dashed") +
+  annotate("text", x = 300, y = -85, label = paste("N[E] == ", MLE_Ne_McCrone_freq0.005), parse = TRUE, color = "red", size = 6) +
+  annotate("rect", xmin = as.numeric(CI_low_Ne_freq0.005), xmax = as.numeric(CI_high_Ne_freq0.005), 
+           ymin = -Inf, ymax = Inf, alpha = 0.1,fill = "black") +
+  labs(x = expression(N[E]), y = "Log-likelihood", title = expression(paste(threshold, "= 0.5%"))) 
+
+fig3d <- ggplot(McCrone_tot_obs_freq0.02_gen1, aes(x = DPS, y = freq)) +
+  geom_point(aes(color = type, shape = type), alpha = 0.5) +
+  geom_line(aes(group = ID), alpha = 0.3) +
+  geom_hline(yintercept = 0.02, color = "red", linetype = "dashed", ) +
+  scale_color_discrete_sequential(palette = "YlGnBu", nmax = 3, rev = FALSE) +
+  labs(x = "Days post symptom onset", y = "iSNV frequency", color = NULL, shape = NULL) +
+  theme(legend.position = c(0.25, 0.9)) +
+  ylim(0, 0.4) +
+  xlim(-2, 6)
+
+fig3e <- ggplot(LLdf_McCrone_freq0.02_gen1) +
+  geom_line(aes(x = Ne, y = prob)) +
+  geom_vline(xintercept = MLE_Ne_McCrone_freq0.02_gen1, color = "red") +
+  geom_hline(yintercept = max(LLdf_McCrone_freq0.02_gen1$prob), color = "red", linetype = "dashed") +
+  annotate("text", x = 100, y = -90, label = paste("N[E] == ", MLE_Ne_McCrone_freq0.02_gen1), parse = TRUE, color = "red", size = 6) +
+  annotate("rect", xmin = as.numeric(CI_low_Ne_freq0.02_gen1), xmax = as.numeric(CI_high_Ne_freq0.02_gen1), 
+           ymin = -Inf, ymax = Inf, alpha = 0.1,fill = "black") +
+  labs(x = expression(N[E]), y = "Log-likelihood", title = expression(paste(threshold, "= 2%"))) 
+
+fig3f <- ggplot(LLdf_McCrone_freq0.005_gen1) +
+  geom_line(aes(x = Ne, y = prob)) +
+  geom_vline(xintercept = MLE_Ne_McCrone_freq0.005_gen1, color = "red") +
+  geom_hline(yintercept = max(LLdf_McCrone_freq0.005_gen1$prob), color = "red", linetype = "dashed") +
+  annotate("text", x = 30, y = -450, label = paste("N[E] == ", MLE_Ne_McCrone_freq0.005_gen1), parse = TRUE, color = "red", size = 6) +
+  annotate("rect", xmin = as.numeric(CI_low_Ne_freq0.005_gen1), xmax = as.numeric(CI_high_Ne_freq0.005_gen1), 
+           ymin = -Inf, ymax = Inf, alpha = 0.1,fill = "black") +
+  labs(x = expression(N[E]), y = "Log-likelihood", title = expression(paste(threshold, "= 0.5%")))
+
+# 7. Put graphs together ====
+fig3 <- fig3a + fig3b + fig3c + fig3d + fig3e + fig3f +
+  plot_layout(ncol = 3) +
+  plot_annotation(tag_levels = "A") &
+  theme(plot.tag = element_text(face = "bold", size = 18))
+
+ggsave(fig3, file = "fig3.pdf", path = "02_plots", width = 15, height = 8)
